363d7c67271a4f0ee273c49418e9247c3d6b14bb
Ann <anngineer@gmail.com>
2022-04-14 18:12:32 -0400

    Adds gh pages info

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
 
   return (
     <div className="App">
+      basename={`/resource-hub` || "" || window.location.pathname}
       <BrowserRouter>
         <ScrollToTop>
           <Routes>
